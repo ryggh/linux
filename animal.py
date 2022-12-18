38886203cb14df904c822f0da816e9aed4aa2bcc
Dong Yuhang <dongyuhang85@gmail.com>
2022-12-18 09:09:11 +0800

    add cat functionality

--- animal.py
+++ animal.py
@@ -1,7 +1,13 @@
 import sys
 
+def cat():
+    print('mewmew')
+
 def main():
-    default()
+    if sys.argv[1] == "cat":
+        cat()
+    else:
+        default()
 
 def default():
     print('hello,dyh')
